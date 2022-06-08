2de259b574b9b69342d1eb2cde0c39e317fa84ca
manuelgalingging <75519823+manuelgalingging@users.noreply.github.com>
2022-06-08 21:25:02 +0700

    Update loaningpage_pengunjung.blade.php (#154)

--- apps/siper/resources/views/pengunjung/loaningpage_pengunjung.blade.php
+++ apps/siper/resources/views/pengunjung/loaningpage_pengunjung.blade.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/loaningpage_pengunjung.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    
+
     <title>Peminjaman Buku</title>
   </head>
   <body>
@@ -18,14 +18,14 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
               aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span> </button>
-  
+
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="home">Home</a>
             </li>
-                
-  
+
+
                        <li class="nav-item dropdown">
                           <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">{{Auth::user()->username}}</a>
                          <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
@@ -43,7 +43,7 @@
     </nav>
   <!-- body -->
 <center>
-  <h2 class="title"> Peminjaman Buku  </h2> 
+  <h2 class="title"> Peminjaman Buku  </h2>
 <hr class="lines">
 <br>
 
@@ -51,78 +51,78 @@
     <div class="wrap">
          <table>
             <tr>
-                <td rowspan="8" width="100px"> <img src="/images/book1.jpeg" width="200px" style="display: block;border-radius: 5%;border-color:white;margin-right:30px" border="2px" ></td>
+                <td rowspan="8" width="100px"> <img src="/img/book.png" width="200px" style="display: block;border-radius: 5%;border-color:white;margin-right:30px" border="2px" ></td>
             </tr>
             <tr>
                 <td><b>Judul Buku </b></td>
-                <td>:</td> <td> The Midnight Library </td>
+                <td>:</td> <td> {{$buku->judul}} </td>
             </tr>
             <tr>
-                <td><b>Pengarang</b></td><td>:</td> <td>Matt Haig</td>
+                <td><b>Pengarang</b></td><td>:</td> <td>{{$buku->pengarang}}</td>
             </tr>
             <tr>
                 <tr>
-                    <td><b> Penerbit </b></td><td>:</td> <td> Mizan</td>
+                    <td><b> Penerbit </b></td><td>:</td> <td> {{$buku->penerbit}}</td>
                 </tr>
             <tr>
-                <td><b>Tahun Terbit </b></td><td>:</td> <td>2015</td>
+                <td><b>Tahun Terbit </b></td><td>:</td> <td>{{$buku->tahunterbit}}</td>
             </tr>
         </table>
         </center>
 <br> <br>
 
 <!-- form peminjaman Input Tanggal Peminjaman-->
-<form action="{{url('/pinjam/'.$buku->id)}}" method="POST">
-  @csrf
-  <div class="row">
-      <div class="col-4"></div>
-      <div class="col-4">
-          <label for="inputTanggalAwal" class="form-label" >Masukkan tanggal Peminjaman</label>
-          <input type="date" name="tanggal_peminjaman" value="ok" class="form-control" id="inputTanggalAwal" aria-describedby="tanggalAwalHelp">
+        <form action="{{url('/pinjam/'.$buku->id)}}" method="POST">
+            @csrf
+            <div class="row">
+                <div class="col-4"></div>
+                <div class="col-4">
+                    <label for="inputTanggalAwal" class="form-label" >Masukkan tanggal Peminjaman</label>
+                    <input type="date" name="tanggal_peminjaman" value="ok" class="form-control" id="inputTanggalAwal" aria-describedby="tanggalAwalHelp">
 
-          <label for="inputTanggalAkhir" class="form-label" >Masukkan tanggal Pengembalian</label>
-          <input type="date" name="tanggal_pengembalian" class="form-control" id="inputTanggalAkhir" aria-describedby="tanggalAkhirHelp">
+                    <label for="inputTanggalAkhir" class="form-label" >Masukkan tanggal Pengembalian</label>
+                    <input type="date" name="tanggal_pengembalian" class="form-control" id="inputTanggalAkhir" aria-describedby="tanggalAkhirHelp">
 
-          <button class="btn btn-primary" type="submit">Ajukan</button>
-      </div>
-  </div>
+                    <button class="btn btn-primary" type="submit">Ajukan</button>
+                </div>
+            </div>
 
-</form>
-  {{-- <center>
-      <div class="row">
-          <div class="col-6">
-              <div class="mb-4">
+        </form>
+            {{-- <center>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="mb-4">
 
-                  <!-- <div id="tanggalAwalHelp" class="form-text text-danger">pesan kesalahan</div> -->
-              </div>
-          </div>
-          <div class="col-6">
-              <div class="mb-4">
+                            <!-- <div id="tanggalAwalHelp" class="form-text text-danger">pesan kesalahan</div> -->
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="mb-4">
 
-                  <!-- <div id="tanggalAkhirHelp" class="form-text text-danger">pesan kesalahan</div> -->
-              </div>
-          </div>
-      </div>
-      <center>
-      <h3 class="title"> Ajukan Peminjaman ?  </h3>
-      {{-- <div class="button-pengajuan">
-                  <a type="submit" class="btn btn-primary"> Ajukan </a>
-                  <a type="button" class="btn btn-secondary" href="home" role="button"> Batalkan </a>
-      </div> --}}
-      {{-- </div> --}}
+                            <!-- <div id="tanggalAkhirHelp" class="form-text text-danger">pesan kesalahan</div> -->
+                        </div>
+                    </div>
+                </div>
+                <center>
+                <h3 class="title"> Ajukan Peminjaman ?  </h3>
+                {{-- <div class="button-pengajuan">
+                            <a type="submit" class="btn btn-primary"> Ajukan </a>
+                            <a type="button" class="btn btn-secondary" href="home" role="button"> Batalkan </a>
+                </div> --}}
+                {{-- </div> --}}
 
 
-<!--Footer -->
+    <!--Footer -->
 <footer>
-<div class="text-center">
-<div class="container p-2"></div>
-<div class="text-center p-1">
-© 2022 Copyright: SIPER SMA Swasta HKBP 2 Tarutung </div>
-<div class="text-center p-1">
-Contact Us On : xxxxxxxx </div>
-</div>
-</footer>
+  <div class="text-center">
+    <div class="container p-2"></div>
+    <div class="text-center p-1">
+      © 2022 Copyright: SIPER SMA Swasta HKBP 2 Tarutung </div>
+    <div class="text-center p-1">
+      Contact Us On : xxxxxxxx </div>
+  </div>
+  </footer>
 
-</body>
+  </body>
 
-</html>
+  </html>
