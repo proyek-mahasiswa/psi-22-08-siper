d19d4a05b2346d93dd306fd1f8659e6bb37b692d
AgnesManalu <89454954+agnesmanalu29@users.noreply.github.com>
2022-06-08 22:34:51 +0700

    Update Peminjaman.php

--- apps/siper/app/Models/Peminjaman.php
+++ apps/siper/app/Models/Peminjaman.php
@@ -10,4 +10,12 @@ class Peminjaman extends Model
     use HasFactory;
     protected $table = "peminjamans";
     protected $fillable = ['user_id', 'buku_id', 'tanggal_pinjam', 'tanggal_kembali', 'status'];
+
+    public function user(){
+        return $this->belongsTo(User::class,'user_id','id');
+    }
+
+    public function buku(){
+        return $this->belongsTo(Buku::class,'buku_id','id');
+    }
 }
