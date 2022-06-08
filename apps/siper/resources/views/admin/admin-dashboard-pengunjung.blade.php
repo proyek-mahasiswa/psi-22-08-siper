c380c6aeda3872b27429cdf7315895634b91b51c
ignatiahutagalung <hutagalungignatia@gmail.com>
2022-06-08 21:02:35 +0700

    Update Halaman Admin Dashboard Pengunjung
    Update Halaman Admin Dashboard Pengunjung

--- apps/siper/resources/views/admin/admin-dashboard-pengunjung.blade.php
+++ apps/siper/resources/views/admin/admin-dashboard-pengunjung.blade.php
@@ -183,23 +183,23 @@
   
 
     <!--Modals Hapus-->
-                            <div class="modal fade" id="hapuspengunjung" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Hapus Data</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                  <label for="">Apakah data ingin dihapus?</label>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                                    <button type="button" class="btn btn-danger">Hapus</button>
-                                </div>
-                                </div>
-                            </div>
-                            </div>
+    <div class="modal fade" id="hapuspengunjung" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Hapus Data</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+            <div class="modal-body">
+                <label for="">Apakah data ingin dihapus?</label>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-danger">Hapus</button>
+            </div>
+            </div>
+        </div>
+    </div>
     
 
 </body>
