e2b2dc72d0fa43ec543d154df2b58eb38ec9421a
Ardika Solagratia Sianturi <78130793+ardikasolagratia@users.noreply.github.com>
2022-06-14 11:08:39 +0700

    update-loaningpage_pengunjung (#173)

--- apps/siper/resources/views/pengunjung/loanhistory-pengunjung.blade.php
+++ apps/siper/resources/views/pengunjung/loanhistory-pengunjung.blade.php
@@ -14,7 +14,7 @@
     <!-- navbar -->
     <nav class="navbar navbar-expand-lg navbar-white bg-blue fixed-top">
       <div class="container-fluid">
-        <a class="navbar-brand" href="home">SIPER</a>
+        <a class="navbar-brand" href="/home">SIPER</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
               aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span> </button>
@@ -22,7 +22,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="home">Home</a>
+              <a class="nav-link active" aria-current="page" href="/home">Home</a>
             </li>
              
                     <li class="nav-item dropdown">
